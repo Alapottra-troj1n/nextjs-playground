e6cdbc6df27bf7160f7aa0829849a4509dcb7e5d
Alapottra <alapottratrojon@gmail.com>
2022-06-25 11:22:08 +0600

    how to pass props from static to dynamic component and making it highly reuseable by framework exportable components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import type { InferGetStaticPropsType } from 'next';
+import getAllProducts from '../frameworks/shopify/getAllProducts';
 
 export async function getStaticProps() {
-  const products = [1,2,3]
+
+  const products = await getAllProducts();
 
   return {
     props: {
@@ -17,14 +19,13 @@ export async function getStaticProps() {
 }
 
 
-export default function Home( {products} :InferGetStaticPropsType<typeof getStaticProps> ){
-
+export default function Home( {products} : InferGetStaticPropsType <typeof getStaticProps> ){
 
 
   return (
     <div>
 
-          <h2>{products}</h2>
+        <h2>{products}</h2>
           
 
     </div>
